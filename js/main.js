5a806fb60e6df258f985adc40be2d9d9ae6376d4
Adalaber <maguado85@gmail.com>
2017-12-17 22:34:53 +0100

    header + logos + cover + burger not work

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// burger
+
+function openClose(idContent) {
+  var burger = document.getElementById(idContent);
+  if (burger.style.display == 'block'){
+			burger.style.display ='none';
+	} else {
+		burger.style.display = 'block';
+	}
+}
